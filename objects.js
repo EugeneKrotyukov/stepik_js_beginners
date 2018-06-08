ecc0bd9911aafd33aa410377fc511264354429d7
EugeneKrotyukov <krotyukov@gmail.com>
2018-06-08 16:33:41 +0300

    array

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-STEPIK JavaScript для начинающих
+// STEPIK JavaScript для начинающих
 
 // ОБЪЕКТЫ
 
@@ -18,3 +18,23 @@ person.sayAll = function() {
     console.log(i + " is " + this[i]);
   }
 }
+// person.sayAll();
+
+
+/*
+вернуть из функции суммарную длину двух строк
+*/
+function testStr(a, b) {
+	return a.length + b.length;
+}
+// console.log(testStr('5794675', '181683'));
+
+
+/*
+передаются два массива случайной длины заполненные случайными числами. 
+Сосчитать сумму всех элементов обоих массивов и возвратить ее из функции.
+*/
+function testArray(a, b) {
+    return a + b;
+} 
+console.log(testStr([8, 1, 1, 7, 4, 0],  [5, 8, 5, 4, 8]));
